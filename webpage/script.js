0f524ef58491799a369e1be1fc940c7a20a8cbc7
SiposLevente <sipi.levente@gmail.com>
2022-04-12 11:05:38 +0200

    data can now only be sent when connected to the mqtt server

--- webpage/script.js
+++ webpage/script.js
@@ -1,7 +1,7 @@
 counter = 1;
 
 mainTopic = "webtest/";
-
+connected = false
 hostname = "broker.emqx.io";
 port = 8084;
 clientid = "test-client";
@@ -14,6 +14,7 @@ var options = {
     //Gets Called if the connection has successfully been established
     onSuccess: function () {
         console.log("Connected!")
+        connected = true
     },
 
     //Gets Called if the connection could not be established
@@ -25,24 +26,30 @@ var options = {
 client.connect(options);
 
 function handleOrientation(event) {
-    document.getElementById("grav_x").innerHTML = "y: " + event.alpha;
-    document.getElementById("grav_y").innerHTML = "z: " + event.beta;
-    document.getElementById("grav_z").innerHTML = "x: " + event.gamma;
-    sendData(event.alpha, "orientation/y");
-    sendData(event.beta, "orientation/z");
-    sendData(event.gamma, "orientation/x");
+    if (connected) {
+        document.getElementById("grav_x").innerHTML = "y: " + event.alpha;
+        document.getElementById("grav_y").innerHTML = "z: " + event.beta;
+        document.getElementById("grav_z").innerHTML = "x: " + event.gamma;
+        sendData(event.alpha, "orientation/y");
+        sendData(event.beta, "orientation/z");
+        sendData(event.gamma, "orientation/x");
+    }
 };
 
 function sendData(data, subtopic) {
-    var message = new Paho.Message("" + data);
-    message.destinationName = mainTopic + "" + subtopic;
-    message.qos = 0;
-    client.send(message);
+    if (connected) {
+        var message = new Paho.Message("" + data);
+        message.destinationName = mainTopic + "" + subtopic;
+        message.qos = 0;
+        client.send(message);
+    }
 }
 
 function incrementCounter() {
     counter += 1;
-    sendData(counter, "counter")
+    if (connected) {
+        sendData(counter, "counter")
+    }
 }
 
 function update_all() {
