a611d7a651297cd22df05c7c061e89b9ce4ad86c
Sipos_Levente_Szabolcs <siposleventeszabolcs@stud.uni-obuda.hu>
2022-04-16 15:25:33 +0200

    reverted changes

--- ros/mqtt_subscribe_publish_move.py
+++ ros/mqtt_subscribe_publish_move.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 import paho.mqtt.client as mqttClient
 import time
 import certifi
@@ -5,113 +6,94 @@ import rospy
 from geometry_msgs.msg import Twist
 from nav_msgs.msg import Odometry
 
-TIMEOUT_TIME = 1
-
-class Orientation:
-    x = 0.0
-    y = 0.0
-    z = 0.0
-
-class Platypous_Controller:
+class Main:
     def __init__(self):
         rospy.init_node('mqtt_subscriber', anonymous=True)
-        rospy.sleep(1)
-        rospy.Subscriber("/driver/wheel_odometry",
-                         Odometry, self.cb_odometry_cp)
         self.twist_pub = rospy.Publisher('/cmd_vel/nav', Twist, queue_size=10)
-        self.main_topic = "controller/"
-        self.x_topic = "controller/orientation/x"
-        self.y_topic = "controller/orientation/y"
-        self.z_topic = "controller/orientation/z"
-        self.use_topic = "controller/inUse"
-        self.platypous_topic = "platypous/"
-        self.connected = False
-        self.timeout = TIMEOUT_TIME
+        rospy.Subscriber("/odometry/wheel", Odometry, self.cb_odometry_cp)
 
     def cb_odometry_cp(self, msg):
         self.odometry_cp = msg
 
-    def on_connect(self, client, userdata, flags, rc):
-        if rc == 0:
-            print("Connected to broker")
-            self.connected = True  # Signal connection
-            client.subscribe(self.x_topic)
-            client.subscribe(self.y_topic)
-            client.subscribe(self.z_topic)
-            client.subscribe(self.use_topic)
-        else:
-            print("Connection failed")
 
-        while self.connected != True:  # Wait for connection
+    def on_connect(client, userdata, flags, rc):    
+        if rc == 0:    
+            print("Connected to broker")    
+            global Connected                #Use global variable
+            Connected = True                #Signal connection
+            client.subscribe("xAccel")
+            client.subscribe("yAccel")
+            client.subscribe("zAccel")
+        else:    
+            print("Connection failed")
+        
+        while Connected != True:    #Wait for connection
             time.sleep(0.1)
 
-    # The callback for when a PUBLISH message is received from the server.
-    def on_message(self, client, userdata, msg):
-        if self.x_topic in msg.topic:
-            Orientation.x = self.get_val_from_message(msg.payload)
-        elif self.y_topic in msg.topic:
-            Orientation.y = self.get_val_from_message(msg.payload)
-        elif self.z_topic in msg.topic:
-            Orientation.z = self.get_val_from_message(msg.payload)
-        elif self.use_topic in msg.topic:
-            msg = self.get_val_from_message(msg.payload)
-            if msg == "released":
-                Orientation.x = 0
-                Orientation.y = 0
-                Orientation.z = 0
-            else:
-                self.timeout = TIMEOUT_TIME
+    def on_message(client, userdata, msg):  # The callback for when a PUBLISH message is received from the server.
+        if "xAccel" in msg.topic:
+            x_value = msg.payload
+            Values.x_value = str(x_value).split("b'")[1].split("'")[0]
+        elif "yAccel" in msg.topic:
+            y_value = msg.payload
+            Values.y_value = str(y_value).split("b'")[1].split("'")[0]
+        elif "zAccel" in msg.topic:
+            z_value = msg.payload
+            Values.z_value = str(z_value).split("b'")[1].split("'")[0]
 
-    def get_val_from_message(self, value):
-        return str(value).split("b'")[1].split("'")[0]
+    def pub(self):
+        vel_msg = Twist()
+        global Connected
+        Connected = False   #global variable for the state of the connection
+        
+        broker_address= "d6b5f80560aa4c889516ecf66760dcae.s2.eu.hivemq.cloud"
+        port = 8883
+        user = "Ponti"
+        password = "n3vSLVTpecC!QhP"
 
-    def start(self):
-        self.connected = False
-
-        broker_address = "192.168.1.150"
-        port = 1883
-        # user = "Ponti"
-        # password = "n3vSLVTpecC!QhP"
-
-        client = mqttClient.Client("Python")  # create new instance
-        # client.tls_set(certifi.where())
-        # set username and password
-        #client.username_pw_set(user, password=password)
-        client.on_connect = self.on_connect  # attach function to callback
-        client.connect(broker_address, port=port)  # connect to broker
-
-        client.loop_start()  # start the loop
+        client = mqttClient.Client("Python")               #create new instance
+        client.tls_set(certifi.where())
+        client.username_pw_set(user, password=password)    #set username and password
+        client.on_connect = Main.on_connect                      #attach function to callback
+        client.connect(broker_address, port=port)          #connect to broker
+        
+        client.loop_start()        #start the loop
         while not rospy.is_shutdown():
-            vel_msg = Twist()
-            self.timeout -= 1
-            if self.timeout == 0:
-                Orientation.x = 0
-                Orientation.y = 0
-                Orientation.z = 0
             time.sleep(1)
-            odometry_data = self.odometry_cp
-            positions = str(odometry_data.pose).split()
+            o = self.odometry_cp
+            positions = str(o.pose).split()
+            x_index = positions.index("x:")
+            y_index = positions.index("y:")
+            z_index = positions.index("z:")
 
-            self.public_platypous_positon("x", positions, client)
-            self.public_platypous_positon("y", positions, client)
-            self.public_platypous_positon("z", positions, client)
+            x = positions[x_index+1]
+            y = positions[y_index+1]
+            z = positions[z_index+1]
 
-            client.on_message = self.on_message
+            f = "from_platypous/"
 
-            print("X: " + str(Orientation.x) + " Y: " + str(Orientation.y) + " Z: " + str(Orientation.z))
+            client.publish(f + positions[x_index].split(":")[0], x)
+            client.publish(f + positions[y_index].split(":")[0], y)
+            client.publish(f + positions[z_index].split(":")[0], z)
 
-            if abs(float(Orientation().z)) > 15:
-                vel_msg.linear.x = -float(Orientation.z)/50
-            if abs(float(Orientation().x)) > 15:
-                vel_msg.angular.z = -float(Orientation.x)/50
+            client.on_message = Main.on_message
+            print("X: " + str(Values.x_value) + " Y: " + str(Values.y_value) + " Z: " + str(Values.z_value))
+            vel_msg.linear.x = float(Values.y_value)/10
+            vel_msg.angular.z = float(Values.x_value)/10
             self.twist_pub.publish(vel_msg)
+            
+class Values:
+    x_value = 0.0
+    y_value = 0.0
+    z_value = 0.0
 
-    def public_platypous_positon(self, value, positions, client):
-        value_index = positions.index(value + ":")
-        value = positions[value_index+1]
-        client.publish(self.platypous_topic +
-                       positions[value_index].split(":")[0], value)
 
 if __name__ == '__main__':
-    controller = Platypous_Controller()
-    controller.start()
+    main = Main()
+    main.pub()
+    
+
+
+    #source ~/catkin_ws/devel/setup.bash
+    #clear
+    #rosrun ros_project mqtt.py
