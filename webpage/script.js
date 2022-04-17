7eff657ca30a0ac53c2cd8dc5c22026e9fcc4d67
Sipos_Levente_Szabolcs <siposleventeszabolcs@stud.uni-obuda.hu>
2022-04-17 18:47:35 +0200

    refactored code

--- webpage/script.js
+++ webpage/script.js
@@ -1,11 +1,21 @@
+const RELEASE_TEXT = "released"
 const TIMEOUT_VAL = 100;
+const hostname = "192.168.1.150";
+const port = 9001;
+const mainTopic = "controller/";
+const useTopic = "inUse";
+const xTopic = "orietation/x";
+const yTopic = "orietation/y";
+const zTopic = "orietation/z";
+const counterTopic = "counter";
+
+const dotId = "dot";
+const controllerButtonId = "controller";
+const controllerBorderId = "controll_border";
 
 counter = 1;
 userId = uuidv4();
 clientId = userId;
-hostname = "192.168.1.150";
-mainTopic = "controller/";
-port = 9001;
 alphaOffset = 0;
 betaOffset = 0;
 gammaOffset = 0;
@@ -26,7 +36,7 @@ var options = {
     onSuccess: function () {
         console.log("Connected!")
         connected = true
-        client.subscribe(mainTopic + "inUse")
+        client.subscribe(mainTopic + useTopic)
     },
     //Gets Called if the connection could not be established
     onFailure: function (message) {
@@ -42,25 +52,28 @@ setInterval(updateAll, 10);
 function buttonPressed() {
     capturingData = !capturingData
     if (capturingData) {
-        document.getElementById("controller").innerText = "Release";
+        document.getElementById(controllerButtonId).innerText = "Release";
+        changeControllerDotVisibility(true);
     } else {
-        document.getElementById("controller").innerText = "Take Control";
+        document.getElementById(controllerButtonId).innerText = "Take Control";
         firstCapture = true;
-        sendData("released", "inUse");
+        changeControllerDotVisibility(false);
+        sendData(RELEASE_TEXT, useTopic);
     }
 }
 
 client.onMessageArrived = function (message) {
-    if (message.payloadString != userId && message.payloadString != "released") {
+    if (message.payloadString != userId && message.payloadString != RELEASE_TEXT) {
         lockController();
-    } else if (message.payloadString == "released") {
+    } else if (message.payloadString == RELEASE_TEXT) {
         releaseController();
     }
 }
 
 function lockController() {
-    document.getElementById("controller").disabled = true;
-    document.getElementById("controller").innerText = "In use!";
+    document.getElementById(dotId).style.backgroundColor = "grey";
+    document.getElementById(controllerButtonId).disabled = true;
+    document.getElementById(controllerButtonId).innerText = "In use!";
     console.log("Other device is using the controller");
     inUse = true;
     useTimeout = TIMEOUT_VAL;
@@ -68,14 +81,14 @@ function lockController() {
 
 function releaseController() {
     console.log("Other device released the controller");
-    document.getElementById("controller").disabled = false;
-    changeControllerDotVisibility(false);
+    document.getElementById(dotId).style.backgroundColor = "red";
+    document.getElementById(controllerButtonId).disabled = false;
     text = "Take Control"
     if (capturingData) {
         text = "Release"
     }
 
-    document.getElementById("controller").innerText = text;
+    document.getElementById(controllerButtonId).innerText = text;
     inUse = false;
 }
 
@@ -83,7 +96,6 @@ function handleOrientation(event) {
     if (connected && capturingData && !inUse) {
         if (firstCapture) {
             firstCapture = false;
-            changeControllerDotVisibility(true);
             alphaOffset = convertValue(event.alpha);
             betaOffset = -convertValue(event.beta);
             gammaOffset = -convertValue(event.gamma);
@@ -92,52 +104,60 @@ function handleOrientation(event) {
         alpha = calculateRealOffset(event.alpha, alphaOffset);
         beta = calculateRealOffset(-event.beta, betaOffset);
         gamma = calculateRealOffset(-event.gamma, gammaOffset);
-        console.log(gamma);
         moveDot(gamma, -beta);
 
-        sendData(userId, "inUse");
-        sendData(alpha, "orientation/y");
-        sendData(beta, "orientation/z");
-        sendData(gamma, "orientation/x");
+        sendData(userId, useTopic);
+        sendData(alpha, yTopic);
+        sendData(beta, zTopic);
+        sendData(gamma, xTopic);
     }
 };
 
-function changeControllerDotVisibility(visible) {
-    if (visible) {
-        document.getElementById("dot").style.display = "block";
-    } else {
-        document.getElementById("dot").style.display = "none";
+function calculateRealOffset(realValue, offset) {
+    return (convertValue((realValue + offset) % 360)).toFixed(2);
+}
+
+function convertValue(value) {
+    modifiedValue = value % 180;
+    if (value > 179) {
+        modifiedValue = -180 + modifiedValue;
     }
+    return -modifiedValue
 }
 
 function moveDot(xOffset, yOffset) {
-    dot = document.getElementById("dot")
+    dot = document.getElementById(dotId)
     dot_radius = parseInt(dot.style.height);
-    rect_size = parseInt(document.getElementById("controll_border").style.height.replace("px", ""));
+    rect_size = parseInt(document.getElementById(controllerBorderId).style.height.replace("px", ""));
     baseX = (rect_size / 2) - dot_radius / 2;
-    baseY = (rect_size / 2) - dot_radius / 2;
+    baseY = baseX;
     dot.style.left = offsetDot(baseX, xOffset, { minVal: 0, maxVal: rect_size - dot_radius }) + "px"
     dot.style.bottom = offsetDot(baseY, yOffset, { minVal: 0, maxVal: rect_size - dot_radius }) + "px"
-    
+
+}
+
+function changeControllerDotVisibility(visible) {
+    if (visible) {
+        document.getElementById(dotId).style.display = "block";
+    } else {
+        document.getElementById(dotId).style.display = "none";
+    }
 }
 
 function offsetDot(base, offset, treshold) {
     product = Number(base + parseInt(offset * 2));
     if (product < treshold.minVal) {
-        
         return treshold.minVal;
     } else if (product > treshold.maxVal) {
-
         return treshold.maxVal;
     }
-
     return product;
 }
 
 function sendData(data, subtopic) {
-    if (data == "released" || (connected && capturingData && !inUse)) {
+    if (data == RELEASE_TEXT || (connected && capturingData && !inUse)) {
         var message = new Paho.Message("" + data);
-        message.destinationName = mainTopic + "" + subtopic;
+        message.destinationName = mainTopic + subtopic;
         message.qos = 0;
         client.send(message);
     }
@@ -152,7 +172,7 @@ function incrementCounter() {
     }
     if (connected && capturingData && !inUse) {
         counter += 1;
-        sendData(counter, "counter")
+        sendData(counter, counterTopic)
     }
 }
 
@@ -164,16 +184,4 @@ function uuidv4() {
     return ([1e7] + -1e3 + -4e3 + -8e3 + -1e11).replace(/[018]/g, c =>
         (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
     );
-}
-
-function convertValue(value) {
-    modifiedValue = value % 180;
-    if (value > 180) {
-        modifiedValue = -180 + modifiedValue;
-    }
-    return -modifiedValue
-}
-
-function calculateRealOffset(realValue, offset) {
-    return (convertValue((realValue + offset) % 360)).toFixed(2);
 }
