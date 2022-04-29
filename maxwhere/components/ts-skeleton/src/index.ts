32454239794b64a2421fa8981da9681aaf186516
SiposLevente <sipos.levente.szabolcs@gmail.com>
2022-04-29 20:08:54 +0200

    minor changes

--- maxwhere/components/ts-skeleton/src/index.ts
+++ maxwhere/components/ts-skeleton/src/index.ts
@@ -3,14 +3,14 @@
  * Proprietary and confidential
  */
 
-import Config from '../src/mqtt-config.json';
+import Config from '../src/mqtt-config.json'
 import { wom, context, modules, Mesh, Node } from 'maxwhere'
 import { womAsync } from '@mxw/next'
-import * as THREE from '@mxw/three';
+import * as THREE from '@mxw/three'
 import { ipcMain } from 'electron'
 import mqtt, { Client, IClientOptions, MqttClient } from 'mqtt'
 
-let currentPos = { 'x': 0, 'y': 0, 'z': 0 } // Initial position of the test avatar
+let currentPos = { 'x': 0, 'y': 0, 'z': 0 }
 let currentOri = new THREE.Quaternion(0, 0, 0, 1)
 
 const mainTopic = 'platypous/'
@@ -26,12 +26,9 @@ const orientationZTopic = orientationTopic + 'z'
 const orientationWTopic = orientationTopic + 'w'
 
 const mqttOptions: IClientOptions = {
-  'host': Config.host,
   'port': Config.port,
   'username': Config.username,
   'password': Config.password,
-  clientId: 'MaxwhereListener',
-  'protocol': 'mqtt',
   'rejectUnauthorized': false,
   'clean': true,
   'keepalive': 60,
@@ -44,64 +41,63 @@ const mqttOptions: IClientOptions = {
 }
 
 let platypous: Mesh
+let mqttClient: MqttClient
 
-let mqttClient = mqtt.connect(`mqtt://${Config.host}:${Config.port}`, mqttOptions)
+async function initMQTT() {
+  mqttClient = mqtt.connect('ws://192.168.1.150', mqttOptions)
 
-mqttClient.on('connect', () => {
-  mqttClient.subscribe(positionXTopic)
-  mqttClient.subscribe(positionYTopic)
-  mqttClient.subscribe(positionZTopic)
+  mqttClient.on('connect', () => {
+    mqttClient.subscribe(positionXTopic)
+    mqttClient.subscribe(positionYTopic)
+    mqttClient.subscribe(positionZTopic)
+  
+    mqttClient.subscribe(orientationXTopic)
+    mqttClient.subscribe(orientationYTopic)
+    mqttClient.subscribe(orientationZTopic)
+    mqttClient.subscribe(orientationWTopic)
+    mqttClient.publish("test", "test")
+  })
+  
+  
+  mqttClient.on('message', (topic, payload) => {
+    switch (topic) {
+      case positionXTopic:
+        currentPos.x = parseInt(payload.toString())
+        console.log('pos update x')
+        break
+      case positionYTopic:
+        currentPos.y = parseInt(payload.toString())
+        break
+      case positionZTopic:
+        currentPos.z = parseInt(payload.toString())
+        break
+      case orientationXTopic:
+        currentOri.x = parseInt(payload.toString())
+        break
+      case orientationYTopic:
+        currentOri.y = parseInt(payload.toString())
+        break
+      case orientationZTopic:
+        currentOri.z = parseInt(payload.toString())
+        break
+      case orientationWTopic:
+        currentOri.w = parseInt(payload.toString())
+        break
+    }
+  })  
+}
 
-  mqttClient.subscribe(orientationXTopic)
-  mqttClient.subscribe(orientationYTopic)
-  mqttClient.subscribe(orientationZTopic)
-})
+module.exports.init = function () { }
 
-
-mqttClient.on('message', (topic, payload) => {
-  switch (topic) {
-    case positionXTopic:
-      currentPos.x = parseInt(payload.toString())
-      break;
-    case positionYTopic:
-      currentPos.y = parseInt(payload.toString())
-      break;
-    case positionZTopic:
-      currentPos.z = parseInt(payload.toString())
-      break;
-    case orientationXTopic:
-      currentOri.x = parseInt(payload.toString())
-      break;
-    case orientationYTopic:
-      currentOri.y = parseInt(payload.toString())
-      break;
-    case orientationZTopic:
-      currentOri.z = parseInt(payload.toString())
-      break;
-    case orientationWTopic:
-      currentOri.w = parseInt(payload.toString())
-      break;
-  }
-  updatePos();
-  updateOri();
-})
-
-mqttClient.publish("test", 'nodejs mqtt test', { qos: 0, retain: false }, (error) => {
-  if (error) {
-    console.error(error)
-  }
-})
-
-module.exports.init = function () { };
-
-module.exports.done = function (nodeReturnedByRenderFunction: Node) { };
+module.exports.done = function (nodeReturnedByRenderFunction: Node) { }
 
 module.exports.render = function (options: object): any {
-  initAsync();
-  return wom.create('node', {});
-};
+  initAsync()
+  initMQTT()
+  return wom.create('node', {})
+}
 
-module.exports.clear = function () { };
+module.exports.clear = function () { }
 
 function updatePos() {
   platypous.setPosition(currentPos.x, currentPos.y, currentPos.z)
@@ -110,19 +106,27 @@ function updatePos() {
 function updateOri() {
   platypous.setOrientation(currentOri.w, currentOri.x, currentPos.y, currentPos.z)
 }
+
+function updateStatus(){
+  updatePos()
+  updateOri()
+}
+
 async function initAsync() {
   platypous = wom.create('mesh', {
     url: 'penguin.mesh',
-    id: 'pengu',
+    id: 'platypous',
     class: 'animal',
     scale: 10,
-    position: { x: currentPos.x, y: currentPos.y, z: currentPos.z },
+    position: { x: currentPos.x, y: currentPos.y+500, z: currentPos.z },
     orientation: { x: currentOri.x, y: currentOri.y, z: currentOri.z, w: currentOri.w },
     autophysical: true,
     done: (m) => {
-      console.log('DONE');
+      console.log('DONE')
     }
-  });
+  })
 
-  await womAsync.render(platypous);
+  await womAsync.render(platypous)
 }
+
+let periodicUpdate = setInterval(updateStatus, 50)
